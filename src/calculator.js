fead5d8ab55553997bcdb29ef3a61f7925629853
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-10 18:19:22 +0530

    Added option to take operation as argument
    This program can now take mathematical operation as an argument.
    Given output only for the required operation.
    Mod operation can also be added.
    It would be good to add functionality to take work on multiple
    operations at a time.

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,38 @@
 let lib = require('../library/lib_calc.js');
 
+const chooseOperation = function(operation,firstNumber,secondNumber){
+  let message = "";
+  if(operation == "add"){
+    message = lib.add(firstNumber,secondNumber);
+    message = firstNumber+"+"+secondNumber+" = "+message;
+    return message;
+
+  }
+  if(operation == "sub"){
+    message = lib.sub(firstNumber,secondNumber);
+    message = firstNumber+"-"+secondNumber+" = "+message;
+    return message;
+  }
+  if(operation == "multiply"){
+    message = lib.multiply(firstNumber,secondNumber);
+    message = firstNumber+"X"+secondNumber+" = "+message;
+    return message;
+  }
+  if(operation == "divide"){
+    let result = lib.divide(firstNumber,secondNumber);
+    message = result.toFixed(2); 
+    message = firstNumber+"/"+secondNumber+" = "+message;
+    return message;
+  }
+  message = "Operation not found";
+  return message;
+}
+
 const main = function(){
-  let a = +process.argv[2];
-  let b = +process.argv[3];
-  console.log(''+a,'+',b,'=',lib.add(a,b));
-  console.log(''+a,'-',b,'=',lib.sub(a,b));
-  console.log(''+a,'*',b,'=',lib.multiply(a,b));
-  console.log(''+a,'/',b,'=',lib.divide(a,b));
+  let operation = process.argv[2];
+  let firstNumber = +process.argv[3];
+  let secondNumber = +process.argv[4];
+  console.log(chooseOperation(operation,firstNumber,secondNumber));
 }
 
 main();
