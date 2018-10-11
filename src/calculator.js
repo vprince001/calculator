9bcefb097ddff7e4796b8abf8b28e27366b66dbe
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-11 12:00:28 +0530

    CODE REFACTOR
    Now this program will take three arguments and provide the output.
    For example -> 10 + 5
                -> 10 - 5
                -> 10 x 5
                -> 10 / 5
    
    This program will run perfectly for the following operators.
    Mod "%" operation could be added in the next version.
    It would be good to add a functionality of evaluating multiple operators
    at a time.

--- src/calculator.js
+++ src/calculator.js
@@ -1,20 +1,20 @@
 let lib = require('../library/lib_calc.js');
 
-const chooseOperation = function(operation,firstNumber,secondNumber){
+const chooseOperation = function(firstNumber,operator,secondNumber){
   const operations={};
-  operations['addition']=lib.add(firstNumber,secondNumber);
-  operations['substraction']=lib.sub(firstNumber,secondNumber);
-  operations['multiplication']=lib.multiply(firstNumber,secondNumber);
-  operations['division']=(lib.divide(firstNumber,secondNumber)).toFixed(2);
-  let result = operations[operation];
-  return operations;
+  operations['+'] = lib.add(firstNumber,secondNumber);
+  operations['-'] = lib.sub(firstNumber,secondNumber);
+  operations['x'] = lib.multiply(firstNumber,secondNumber);
+  operations['/'] = (lib.divide(firstNumber,secondNumber)).toFixed(2);
+  let result = operations[operator];
+  return result;
 }
 
 const main = function(){
-  let operation = process.argv[2];
-  let firstNumber = +process.argv[3];
+  let firstNumber = +process.argv[2];
+  let operator = process.argv[3];
   let secondNumber = +process.argv[4];
-  console.log(chooseOperation(operation,firstNumber,secondNumber));
+  console.log(chooseOperation(firstNumber,operator,secondNumber));
 }
 
 main();
