c1437e811d965c781973e8ea9b8b6bdca878e030
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-21 11:15:05 +0530

    Move chooseOperation from main to library

--- calculator.js
+++ calculator.js
@@ -1,16 +1,5 @@
 let lib = require('./src/calc_lib.js');
 
-const chooseOperation = function(firstNumber,operator,secondNumber){
-  const operations={};
-  operations['+'] = lib.add(firstNumber,secondNumber);
-  operations['-'] = lib.subtract(firstNumber,secondNumber);
-  operations['x'] = lib.multiply(firstNumber,secondNumber);
-  operations['/'] = (lib.divide(firstNumber,secondNumber)).toFixed(2);
-  operations['%'] = lib.mod(firstNumber,secondNumber);
-  let result = operations[operator];
-  return result;
-}
-
 const main = function(){
   let firstNumber = +process.argv[2];
   let operator = process.argv[3];
--- src/calc_lib.js
+++ src/calc_lib.js
@@ -18,8 +18,20 @@ let mod = function(a,b){
   return a%b;
 };
 
+const chooseOperation = function(firstNumber,operator,secondNumber){
+  const operations={};
+  operations['+'] = lib.add(firstNumber,secondNumber);
+  operations['-'] = lib.subtract(firstNumber,secondNumber);
+  operations['x'] = lib.multiply(firstNumber,secondNumber);
+  operations['/'] = (lib.divide(firstNumber,secondNumber)).toFixed(2);
+  operations['%'] = lib.mod(firstNumber,secondNumber);
+  let result = operations[operator];
+  return result;
+}
+
 exports.add = add;
 exports.subtract = subtract;
 exports.multiply = multiply; 
 exports.divide = divide;
 exports.mod = mod;
+exports.chooseOperation = chooseOperation;
